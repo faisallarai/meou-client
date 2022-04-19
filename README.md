e87397aa13755947815c1753f8cd8804304fc127
Issaka Faisal <faisal@mpharma.com>
2022-04-19 02:56:37 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+## Author: Issaka Faisal
+
+## Getting Started.
+
+<a name="One"></a>
+
+## Design
+
+![alt text](https://lucid.app/publicSegments/view/18ea80c3-8e4c-4c31-95b7-27b836c5f01c/image.png)
+
+<a name="Two"></a>
+
+## Prerequisite:
+
+1. git
+2. yarn
+3. npm
+
+<a name="Three"></a>
+
+### Deploying ReactJS:
+
+1. Run the ff.
+
+   - `cd meou-client`
+   - `git add .`
+   - `git comit -m "initial commit"`
+   - `git tag v1.0.5`
+   - `git push origin main --tag`
+
+<a name="Four"></a>
+
+## Improvement
+
+### Add SNYK step to check vulnerabilities in images
---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import catService from './services/cats';
+
+const App = () => {
+
+  const [imageId, setImageId] = useState('');
+
+  useEffect(() => {
+    catService.getAll().then(res => {
+      console.log(res.data)
+      setImageId(res.data.cat.imageId)
+    })
+  },[])
+
+  return <div>
+    <h1>Hour Cat</h1>
+    {imageId && <img
+        src={`https://meou-api.s3.us-east-2.amazonaws.com/${imageId}.jpeg`}
+        width={260}
+        height={360}
+        alt=""
+      />}
+    
+   
+  </div>;
+};
+
+export default App;
